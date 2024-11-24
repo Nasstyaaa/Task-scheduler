6b6ac07a97e73ad02b7573d6d04a2733f86c53d2
Nasstyaaa <Luk-2004@mail.ru>
2024-11-24 22:18:50 +0300

    refactor: new feature in application.properties

--- backend/src/main/java/org/nastya/backend/controller/TestController.java
+++ backend/src/main/java/org/nastya/backend/controller/TestController.java
@@ -1,22 +1,22 @@
 package org.nastya.backend.controller;
 
 import lombok.AllArgsConstructor;
+import org.nastya.backend.model.User;
 import org.nastya.backend.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/main")
 @AllArgsConstructor
 public class TestController {
 
-    @Autowired
     private final UserRepository userRepository;
 
     @GetMapping("/test")
-    public String test(){
-        return userRepository.findAll().toString();
+    public ResponseEntity<List<User>> test(){
+        return ResponseEntity.ok(userRepository.findAll());
     }
 }
