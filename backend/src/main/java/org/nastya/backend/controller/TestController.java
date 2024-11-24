7e21f760d6533113f2a5627039fb5c5c69857ee3
Nasstyaaa <Luk-2004@mail.ru>
2024-11-24 15:55:03 +0300

    feat(frontend): add nginx in docker file

--- backend/src/main/java/org/nastya/backend/controller/TestController.java
+++ backend/src/main/java/org/nastya/backend/controller/TestController.java
@@ -1,15 +1,22 @@
 package org.nastya.backend.controller;
 
+import lombok.AllArgsConstructor;
+import org.nastya.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/main")
+@RequestMapping("/user")
+@AllArgsConstructor
 public class TestController {
+
+    @Autowired
+    private final UserRepository userRepository;
 
     @GetMapping("/test")
     public String test(){
-        return "tesjhbuit";
+        return userRepository.findAll().toString();
     }
 }
