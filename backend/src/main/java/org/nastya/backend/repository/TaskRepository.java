e19a5f8dcbbb75a0546aebbd63b37c294293be76
Nasstyaaa <Luk-2004@mail.ru>
2024-11-28 16:41:56 +0300

    feat(backend): create repository for tasks

---
+++ backend/src/main/java/org/nastya/backend/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package org.nastya.backend.repository;
+
+import org.nastya.backend.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository  extends JpaRepository<Task, String> {
+}
