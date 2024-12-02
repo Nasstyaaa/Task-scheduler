86ed94cf16b0b82e7972e29aae70af1c49701b67
Nasstyaaa <Luk-2004@mail.ru>
2024-12-02 16:07:47 +0300

    feat(scheduler): add model 'User'

---
+++ scheduler/src/main/java/org/nastya/scheduler/model/User.java
@@ -0,0 +1,25 @@
+package org.nastya.scheduler.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+}
